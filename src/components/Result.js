f2deed3393206b17b10f0f137c018523d3df47d1
Sid-Miram <144012022+Sid-Miram@users.noreply.github.com>
2024-01-11 14:58:03 +0530

    Redefined Result.js
    Made code more clean and readable

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-export default function Result({answer}) {
- let comment;
- if(answer<3){
-  comment = "Kya Kar Raha Hai Bhai Tu!!"
- } else if(answer<5){
-  comment = "Koi Na Bhai, Nice try!!"
- }else if(answer===5){
-  comment="Gangadhar nahi, tu shaktiman h!!"
- }
+export default function Result({ answer }) {
+  let comment;
+  switch (true) {
+    case answer < 3:
+      comment = 'Kya Kar Raha Hai Bhai Tu!!';
+      break;
+    case answer < 5:
+      comment = 'Koi Na Bhai, Nice try!!';
+      break;
+    case answer === 5:
+      comment = 'Gangadhar nahi, tu shaktiman h!!';
+      break;
+    default:
+      comment = 'Some default comment';
+  }
+
+  const precisionPercentage = ((answer / 5) * 100).toFixed(2) + '%';
+
   return (
     <div className='Container2'>
       <p id='result'> Result </p>
-      
       <h2 id='comment'>{comment}</h2>
-      
-      <h2> Precision Percentage: <span id='precision'>{(answer/5)*100}%</span></h2> 
+      <h2> Precision Percentage: <span id='precision'>{precisionPercentage}</span></h2>
     </div>
-  )
+  );
 }
