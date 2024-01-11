725dc01c29dac7a28785b4fa492b09e07207be72
Sid-Miram <144012022+Sid-Miram@users.noreply.github.com>
2024-01-11 13:10:52 +0530

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export default function Result() {
+export default function Result({answer}) {
+ let comment;
+ if(answer<3){
+  comment = "Kya Kar Raha Hai Bhai Tu!!"
+ } else if(answer<5){
+  comment = "Koi Na Bhai, Nice try!!"
+ }else if(answer===5){
+  comment="Gangadhar nahi, tu shaktiman h!!"
+ }
   return (
-    <div>
+    <div className='Container2'>
+      <p id='result'> Result </p>
       
+      <h2 id='comment'>{comment}</h2>
+      
+      <h2> Precision Percentage: <span id='precision'>{(answer/5)*100}%</span></h2> 
     </div>
   )
 }
